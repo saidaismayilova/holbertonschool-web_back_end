dcc72b5652bcc029f5fbe4e61373f4b74af19bb0
saidaismayilova <iseide40@gmail.com>
2025-11-30 20:30:21 +0000

    salam

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+Module documentation: Lists all documents in a MongoDB collection.
+
+This module provides a function to retrieve all documents from a
+MongoDB collection using PyMongo.
+"""
+
+
 def list_all(mongo_collection):
     """
     Lists all documents in a MongoDB collection.
